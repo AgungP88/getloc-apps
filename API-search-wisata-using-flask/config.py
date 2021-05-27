d8468ea96fe33c3ff0b0a74d7f6f598fe7ba5d92
lukymulana <lukymulana@gmail.com>
2021-05-27 16:26:02 +0700

    change url to connect cloud SQL and add variable connection_name

--- API-search-wisata-using-flask/config.py
+++ API-search-wisata-using-flask/config.py
@@ -8,8 +8,9 @@ class Config(object):
     DATABASE = str(os.environ.get("DB_DATABASE"))
     USERNAME = str(os.environ.get("DB_USERNAME"))
     PASSWORD = str(os.environ.get("DB_PASSWORD"))
+    CONNECTION_NAME = str(os.environ.get("DB_CONNECTION_NAME"))
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + \
-        ':' + PASSWORD + '@' + HOST + '/' + DATABASE
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + USERNAME + ':' + PASSWORD + \
+        '@/' + DATABASE + '?unix_socket=/cloudsql/' + CONNECTION_NAME
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
