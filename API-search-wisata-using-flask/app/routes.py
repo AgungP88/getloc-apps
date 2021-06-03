91be9a8aef0fb285dcc4c40c5dccca18e1a0960a
lukymulana <lukymulana@gmail.com>
2021-06-03 09:00:28 +0700

    add route for paket, idtoplaceid and multipleSearch

--- API-search-wisata-using-flask/app/routes.py
+++ API-search-wisata-using-flask/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from app.controller import WisataController
+from app.controller import WisataController, PaketController, IdToPlaceIdController
 from flask import request
 
 
@@ -29,3 +29,45 @@ def searchName():
 def searchCity():
     kota = request.args.get("kota")
     return WisataController.searchCity(kota)
+
+
+@app.route('/wisata/search/place_id', methods=['GET'])
+def MultipleSearchId():
+    # args = {"p1": v1, "p2": None, "p3": v3}
+    args = request.args
+    # filtered_args = {"p1": v1, "p3": v3}
+    filtered_args = {k: v for k, v in args.items() if v is not None}
+
+    array = []
+
+    for i in filtered_args:
+        array.append(" OR place_id = '" + filtered_args[i] + "'")
+
+    s = str(array)
+
+    del1 = s.replace('[', '')
+    del2 = del1.replace(']', '')
+    del3 = del2.replace('"', '')
+    result = del3.replace(',', '')
+
+    return WisataController.multipleSearchId(result)
+
+
+@app.route('/paket', methods=['GET'])
+def indexPaket():
+    return PaketController.index()
+
+
+@app.route('/paket/<id>', methods=['GET'])
+def paketById(id):
+    return PaketController.paketById(id)
+
+
+@app.route('/idtoplaceid', methods=['GET'])
+def idToPlaceIdIndex():
+    return IdToPlaceIdController.index()
+
+
+@app.route('/idtoplaceid/<id>', methods=['GET'])
+def idToPlaceId(id):
+    return IdToPlaceIdController.filterById(id)
