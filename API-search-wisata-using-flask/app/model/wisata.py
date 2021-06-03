49058476a122b800600f2381f55d140f18ec9c87
lukymulana <lukymulana@gmail.com>
2021-06-03 08:58:24 +0700

    add stucture for description

--- API-search-wisata-using-flask/app/model/wisata.py
+++ API-search-wisata-using-flask/app/model/wisata.py
@@ -9,6 +9,7 @@ class Wisata(db.Model):
     price = db.Column(db.String(7), nullable=False)
     rating = db.Column(db.String(5), nullable=False)
     spend_time = db.Column(db.String(5), nullable=False)
+    deskripsi = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
         return '<Wisata {}>'.format(self.name)
